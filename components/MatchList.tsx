af8a614a30933bd0196346c2b0eeffc1d3b053db
Christopher Berisford <christopher.berisford@siemens.com>
2023-10-18 17:19:59 +0100

    Improved match list component

--- components/MatchList.tsx
+++ components/MatchList.tsx
@@ -56,16 +56,48 @@ const MatchList: React.FC<Props> = (props) => (
     {
       props.matches.map((match) => {
         return (
+          // <div className="m-1" key={match.id}>
+          //   <Button variant="outline" size="xl" className="w-full ">
+          //     <div className="grid grid-col-1">
+          //       <div>
+          //         <span className={isHomeTeam(match, props.context) ? "font-extrabold" : "dark:font-extralight"}>{match.home_team.name}</span>
+          //         <span className="px-2">v</span>
+          //         <span className={isHomeTeam(match, props.context) ? "dark:font-extralight" : "font-extrabold"}>{match.away_team.name}</span>
+          //         {resultSymbol(match, props.context)}
+          //       </div>
+          //       <span className="text-primary">    {String(match.division?.name)}</span>
+          //     </div>
+          //   </Button>
+
+          // </div>
           <div className="m-1" key={match.id}>
-            <Button variant="outline" size="xl" className="w-full ">
-              <div className="grid grid-col-1">
-                <div>
-                  <span className={isHomeTeam(match, props.context) ? "font-extrabold" : "dark:font-extralight"}>{match.home_team.name}</span>
-                  <span className="px-2">v</span>
-                  <span className={isHomeTeam(match, props.context) ? "dark:font-extralight" : "font-extrabold"}>{match.away_team.name}</span>
-                  {resultSymbol(match, props.context)}
+            <Button variant="outline" size="xl" className="w-full p-0 pb-2 h-[55px] align-middle overflow-clip flex items-center">
+              <div className="grid grid-cols-2 w-full flex">
+                <div className="bg-gradient-to-r from-green-600/25 dark:bg-green-800/25 to-40% to-background text-left">
+                  <span className="h-full text-9xl font-extrabold opacity-[2%] overflow-clip">WIN</span>
+
                 </div>
-                <span className="text-primary">    {String(match.division?.name)}</span>
+                <div className="bg-gradient-to-r from-background from-60% to-red-800/25 dark:to-red-800/50 p-0 m-0 text-right">
+                  <span className="h-full text-9xl font-extrabold opacity-[2%] overflow-clip">LOSE</span>
+
+                </div>
+
+
+                {/* <span className="px-2">v</span> */}
+
+                {/* {resultSymbol(match, props.context)} */}
+
+                {/* <span className="text-primary">    {String(match.division?.name)}</span> */}
+              </div>
+              <div className="absolute inset grid grid-cols-3 w-full text-foreground/75 flex items-center">
+                <span className={`${isHomeTeam(match, props.context) ? "font-bold" : "dark:font-extralight"} uppercase text-2xl`} >{match.home_team.name}</span>
+                <div className="text-4xl font-extralight">
+                  <span className="text-right">{match.home_rubbers}</span>
+                  <span>-</span>
+                  <span className="text-left">{match.away_rubbers}</span>
+                </div>
+                <span className={`${isHomeTeam(match, props.context) ? "dark:font-extralight" : "font-extrabold"} uppercase text-2xl`} >{match.away_team.name}</span>
+
               </div>
             </Button>
 
